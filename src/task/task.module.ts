056329c9b7fea898ff3edb8aaea7494dd5966a39
przemekr95 <przemekrura95@gmail.com>
2024-01-21 17:04:33 +0100

    Added Tasks Service

--- src/task/task.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TasksController } from 'src/tasks/tasks.controller';
-
-@Module({
-  controllers: [TasksController],
-})
-export class TaskModule { }
